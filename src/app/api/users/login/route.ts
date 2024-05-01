bf05eb89da259c3f065eed03e273bba2aa44d583
HuzaifaJabbar17 <jabbarhuzaifa123@gmail.com>
2024-05-01 13:35:22 +0530

    user verify,login,logout

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,60 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/user.model";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return NextResponse.json(
+        { error: "user does not exist" },
+        { status: 400 }
+      );
+    }
+    console.log("User exists");
+    const validUser = await bcryptjs.compare(user.password, password);
+
+    if (!validUser) {
+      return NextResponse.json(
+        { error: "Invalid User credentials" },
+        { status: 400 }
+      );
+    }
+
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+
+    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
+
+    // here we are not sending we are creating a variable of response
+    const response = NextResponse.json({
+      message: "logged in successfully",
+      success: true,
+    });
+
+    response.cookies.set("token", token, {
+      httpOnly: true,
+    });
+
+    return response;
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error.message },
+      {
+        status: 500,
+      }
+    );
+  }
+}
